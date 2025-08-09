61764e40ec67d0cb7e3a5735b5e2d92ade838023
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-08-09 10:47:01 -0300

    feat: user scope created

---
+++ src/user/user.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@nestjs/common";
+import { User } from "generated/prisma";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class UserService{
+  constructor(private prisma: PrismaService) {}
+}
