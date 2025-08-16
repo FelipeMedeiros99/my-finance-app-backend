27535607463a78359141eaff5ecace6f37c0ac23
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-08-15 21:49:02 -0300

    refactor: testes altered to UserService

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -2,28 +2,36 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { PrismaService } from "../prisma/prisma.service";
 import { UserRepository } from "./user.repository";
 import { faker } from "@faker-js/faker/.";
+import { UserService } from "./user.service";
 
-describe("UserRepository tests", () => {
+describe("User tests", () => {
   let userRepository: UserRepository;
+  let userService: UserService;
   let prisma: PrismaService;
 
   beforeEach(async () => {
     const userTest: TestingModule = await Test.createTestingModule({
       controllers: [],
-      providers: [UserRepository, PrismaService],
+      providers: [UserRepository, UserService, PrismaService],
     }).compile()
 
-    prisma = userTest.get(PrismaService);
-    userRepository = userTest.get(UserRepository);
+    prisma = userTest.get<PrismaService>(PrismaService);
+    userRepository = userTest.get<UserRepository>(UserRepository);
+    userService = userTest.get<UserService>(UserService)
   });
 
   describe("Create user tests", () => {
     it("should create a new user at database", async () => {
+      let password = faker.internet.password();
+
       const userData = {
         email: faker.internet.email(),
-        password: faker.internet.password()
+        password: password,
+        confirmPassword: password
       }
-      await userRepository.create(userData.email, userData.password);
+      await userService.createUser(userData);
+      
+      // await userRepository.create(userData.email, userData.password);
       const findUser = await userRepository.findByEmail(userData.email)
 
       expect(findUser).not.toBeNull()
@@ -32,22 +40,26 @@ describe("UserRepository tests", () => {
   })
 
   it("Should return erro to create duplicate", async()=>{
+    let password = faker.internet.password();
     const userData = {
       email: faker.internet.email(),
-      password: faker.internet.password()
+      password: password,
+      confirmPassword: password
     }
-    await userRepository.create(userData.email, userData.password);
-    expect(userRepository.create(userData.email, userData.password)).rejects.toMatchObject({code: "P2002"})
+    await userService.createUser(userData);
+    expect(userService.createUser(userData)).rejects.toThrow("This email is already registered.")
   })
 
   it("Should find a user by email", async ()=>{
+    let password = faker.internet.password();
     const userData = {
       email: faker.internet.email(),
-      password: faker.internet.password()
+      password: password,
+      confirmPassword: password
     }
 
-    await userRepository.create(userData.email, userData.password);
-    expect(userRepository.findByEmail(userData.email)).resolves.toMatchObject(userData)
+    await userService.createUser(userData);
+    expect(userService.findUserByEmail(userData.email)).resolves.toMatchObject({email: userData.email})
   })
 
 })
