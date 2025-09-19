d6ef1700c8b92ed9ae0490b71a57978ba782ea8e
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-18 23:35:04 -0300

    feat: class validation added

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,35 @@
-export class CreateTransactionDto {}
+import { Type } from "class-transformer";
+import { IsBoolean, IsDate, IsIn, IsNotEmpty, IsNumber, IsString, Matches, MaxLength, Min, MinLength } from "class-validator";
+
+export class CreateTransactionDto{
+  @IsString()
+  @MinLength(3)
+  @MaxLength(30)
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  value: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[\d]{4}-[\d]{1,2}-[\d]{1,2}$/)
+  dueDate: string;
+
+  @IsNotEmpty()
+  @IsIn(["INCOME", "EXPENSE"])
+  type: "INCOME" | "EXPENSE";
+
+  @IsBoolean()
+  @IsNotEmpty()
+  wasConfirm: boolean;
+
+  @IsNumber()
+  @IsNotEmpty()
+  categoryId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  accountId: number;
+}
