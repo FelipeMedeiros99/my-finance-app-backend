13a040410567aaaf14594dcbae6687b4a39f52e6
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-19 16:39:52 -0300

    feat: accounts default to new users

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -7,13 +7,61 @@ export class UserRepository {
   constructor(private readonly prisma: PrismaService) { }
 
   async create(username: string, password: string) {
-    return await this.prisma.user.create({
+    ;
+
+    const newUser = await this.prisma.user.create({
       data: {
         username: username,
         password: password
       }
     })
+
+    await this.prisma.account.createMany({
+      data: [
+        {
+          name: "Poupança",
+          userId: newUser.id
+        },
+        {
+          name: "Carteira",
+          userId: newUser.id
+        }
+      ]
+    })
+    
+    await this.prisma.category.createMany({
+      data: [
+        {
+          name: "Gastos Essenciais",
+          userId: newUser.id,
+          type: 'EXPENSE'
+        },{
+          name: "Gastos Supérfulos",
+          userId: newUser.id,
+          type: 'EXPENSE'
+        },{
+          name: "Investimentos",
+          userId: newUser.id,
+          type: 'EXPENSE'
+        },
+        {
+          name: "Salário",
+          userId: newUser.id,
+          type: 'INCOME'
+        },{
+          name: "Renda extra",
+          userId: newUser.id,
+          type: 'INCOME'
+        },{
+          name: "Benefícios",
+          userId: newUser.id,
+          type: 'INCOME'
+        }
+      ]
+    })
+
   }
+
 
   async findUserByUsername(username: string) {
     return await this.prisma.user.findFirst({
