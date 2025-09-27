c00a734105e4af56a61996e7ea2d66da98a87900
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-09-26 23:16:46 -0300

    fix: PrismaService added at module

--- src/card-payment/card-payment.module.ts
+++ src/card-payment/card-payment.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CardPaymentService } from './card-payment.service';
 import { CardPaymentController } from './card-payment.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [CardPaymentController],
-  providers: [CardPaymentService],
+  providers: [CardPaymentService, PrismaService],
 })
 export class CardPaymentModule {}
